9195716a3b15b0194d760f54dd57af92abf78a06
smartnitesh <niteshkumar503@gmail.com>
2022-07-31 13:28:21 +0530

    datatype

--- JavaScript/DAY-2/level3.js
+++ JavaScript/DAY-2/level3.js
@@ -10,7 +10,34 @@ let count_N='You cannot end a sentence with because because because is a conjunc
 
 console.log(count_N.match('because'))
 
-
+//3
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
-console.log(sentence.match(/\s+/g))
+//let regEx = /d(b+)d/g
+
+//console.log(sentence.match(regEx))
+
+
+//4Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+let text='He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month'
+
+console.log(text.match(/\d+/g)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
